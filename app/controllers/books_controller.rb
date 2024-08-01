33d706cc9a1f6594aa8103c351b53640268b4123
EC2 Default User <ec2-user@ip-172-31-7-210.ap-northeast-1.compute.internal>
2024-08-01 13:48:41 +0000

    Commi

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  
+  def new
+    @book_new = Book.new
+  end
+  
+  def create
+    @book_new = Book.new(book_params)
+    @book_new.user_id = current_user.id
+    @user = current_user
+    @books = Book.all
+    if @book_new.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to @book_new
+    else
+      render :index
+    end
+  end
+  
+  def index
+    @book_new = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def show
+    @book_new = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+end
+
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  
+  has_one_attached :profile_image
+  has_many :books, dependent: :destroy
+  
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
+
+  validates :body, length: { maximum: 50 }
+
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image
+  end
+  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
+
